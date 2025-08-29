5687a236a5195da519f58a45d98231d898f91f04
Juan Zuluaga <jzuluaga55@gmail.com>
2025-08-28 20:19:20 -0500

    Finish class Date

---
+++ POO.Concepts/POO.Concepts.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using POO.Concepts.Core;
+
+try
+{
+    var date1 = new Date(2025, 11, 30);
+    var date2 = new Date(2028, 2, 29);
+
+    Console.WriteLine(date1.ToString());
+    Console.WriteLine(date2.ToString());
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
